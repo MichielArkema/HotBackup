0a41d8d978d0cf22a09ee8cfc439a2825c552baa
MichielArkema <michiel.arkema@gmail.com>
2021-11-06 14:05:28 +0100

    Added help messages. Pre added status and list command.

--- HotBackupFree/src/main/java/nl/michielarkema/hotbackupfree/commands/BackupCommand.java
+++ HotBackupFree/src/main/java/nl/michielarkema/hotbackupfree/commands/BackupCommand.java
@@ -1,14 +1,20 @@
 package nl.michielarkema.hotbackupfree.commands;
 
+import com.coloredcarrot.api.sidebar.Sidebar;
+import com.coloredcarrot.api.sidebar.SidebarString;
+import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import nl.michielarkema.hotbackupfree.HotBackup;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public final class BackupCommand implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(args.length == 0) {
@@ -24,7 +30,25 @@ public final class BackupCommand implements CommandExecutor {
             case "start":
                 this.startCommand(sender, args);
                 break;
+            case "status":
+                this.statusCommand(sender);
+                break;
+            case "list":
+                //Todo: Show inventory screen with a list of backup files.
+                //Todo: Action bar message code: Player#spigot()#sendMessage(ChatMessageType.ACTION_BAR,component);
+                TextComponent txt = new TextComponent(ChatColor.YELLOW + "Hover of an item to see its data.");
+                ((Player)sender).spigot()
+                        .sendMessage(ChatMessageType.ACTION_BAR, txt);
+                break;
         }
+    }
+
+    private void statusCommand(CommandSender sender) {
+        SidebarString[] lines = new SidebarString[] {
+                new SidebarString("")
+        };
+        Sidebar sideBar = new Sidebar(ChatColor.GOLD + "Backup Status", HotBackup.getInstance(), 60, lines);
+        sideBar.showTo((Player) sender);
     }
 
     private void startCommand(CommandSender sender, String[] args) {
@@ -34,11 +58,10 @@ public final class BackupCommand implements CommandExecutor {
         sender.sendMessage(ChatColor.DARK_GRAY + "----------------------------------------------");
         sender.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "HotBackup(Free) Version: 1.0.0-TESTING");
         sender.sendMessage("");
-        sender.sendMessage(ChatColor.GREEN +
-                "Usage: /hbu start - " + ChatColor.GOLD + "Starts a new backup process.");
-        sender.sendMessage(ChatColor.GREEN +
-                "Usage: /hbu status - " + ChatColor.GOLD + "Shows the backup status.");
-
+        sender.sendMessage(ChatColor.GREEN + "Usage: /hbu start - " + ChatColor.GOLD + "Starts a new backup process.");
+        sender.sendMessage(ChatColor.GREEN + "Usage: /hbu status - " + ChatColor.GOLD + "Shows the backup status.");
+        sender.sendMessage(ChatColor.GREEN + "Usage: /hbu list - " + ChatColor.GOLD + "Shows the list of backup files.");
+        sender.sendMessage("");
         TextComponent txt = new TextComponent(ChatColor.LIGHT_PURPLE + "You are using the free version. Get the full version");
         txt.addExtra(" HERE");
         txt.setItalic(true);
